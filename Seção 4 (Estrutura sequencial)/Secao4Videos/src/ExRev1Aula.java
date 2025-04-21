c0369b08b269058588dc8dd6205aa263a5fb9d49
FeitosaJrPc <juniorfeito2016@gmail.com>
2025-04-21 18:05:01 -0300

    final video 26

--- Seção 4 (Estrutura sequencial)/Secao4Videos/src/ExRev1Aula.java
+++ Seção 4 (Estrutura sequencial)/Secao4Videos/src/ExRev1Aula.java
@@ -1,8 +1,11 @@
 //Entrada de dados 
+import java.util.Locale;
 import java.util.Scanner;
 public class ExRev1Aula {
 
 	public static void main(String[] args) {
+		// Serve para ler entradas com ponto. 5.25 
+		Locale.setDefault(Locale.US);
 //		Para criar entrada de dados, precisamos criar um OBJETO do tipo SCANNER da seguinte forma.
 //		Scanner sc = new Scanner(System.in);
 //		Para a entrada de dadods funcionar, precisamos fazer o import do objeto e fechhar a entrada de dados.  
@@ -24,11 +27,19 @@ public class ExRev1Aula {
 		Double d;
 		System.out.println("Agora digite um número com 4 casas decimais");
 		d = sc.nextDouble();
+//		Para ler um caractere x = sc.next().charAt(0);
+		char c;
+		System.out.println("Agora digite um caractere");
+		c = sc.next().charAt(0);
+		
+		
 		
 		System.out.printf("Você digitou: %s%nDepois digitou: %s%n", x, y  );
 		System.out.printf("O número inteiro foi: " + i);
 		System.out.println(d);
 		System.out.printf("Número digitado com 2 casas decimais: %.2f%nNúmero digitado com três casas decimais: %.3f%nNúmero digitado com quatro casas decimais: %.4f%n", d, d, d);
+		System.out.println("Caractere digitado: " + c);
+		
 		sc.close();	
 	}
 }
